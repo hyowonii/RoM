b72940b7792f8192c78cc86e968036eec8f40363
hyowon <hyowon9964@gmail.com>
2022-06-02 00:16:40 +0900

    Feat: select photo 페이지 버튼 클릭 연결

--- app/src/main/java/com/example/roomofmemory/SelectPhoto.java
+++ app/src/main/java/com/example/roomofmemory/SelectPhoto.java
@@ -2,13 +2,27 @@ package com.example.roomofmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectPhoto extends AppCompatActivity {
+
+    Button doneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_photo);
+
+        doneBtn = (Button)findViewById(R.id.doneSelect);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DateDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 }
