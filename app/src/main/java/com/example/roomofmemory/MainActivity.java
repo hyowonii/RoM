516f80453c190ad8a143b5000038816682da1b42
hyowon <hyowon9964@gmail.com>
2022-06-01 23:08:30 +0900

    Design: shape of circle button으로 수정

--- app/src/main/java/com/example/roomofmemory/MainActivity.java
+++ app/src/main/java/com/example/roomofmemory/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.roomofmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.goToPhotoOrDiary);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),PhotoOrDiary.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/roomofmemory/PhotoOrDiary.java
+++ app/src/main/java/com/example/roomofmemory/PhotoOrDiary.java
@@ -1,8 +1,22 @@
 package com.example.roomofmemory;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class PhotoOrDiary extends AppCompatActivity {
 
