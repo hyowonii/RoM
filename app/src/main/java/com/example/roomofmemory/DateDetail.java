e78cee733bf656feb51b844715d5c1741719704f
hyowon <hyowon9964@gmail.com>
2022-06-02 00:44:13 +0900

    Feat: date detail 페이지 플러스 버튼 클릭 연결

--- app/src/main/java/com/example/roomofmemory/DateDetail.java
+++ app/src/main/java/com/example/roomofmemory/DateDetail.java
@@ -2,13 +2,27 @@ package com.example.roomofmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DateDetail extends AppCompatActivity {
+
+    ImageButton addDiary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_detail);
+
+        addDiary = (ImageButton)findViewById(R.id.addDiaryBtn);
+        addDiary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WriteDiary.class);
+                startActivity(intent);
+            }
+        });
     }
 }
