ade3e2adea70dfe8d4fe11aa6720172cd03ea904
hyowon <hyowon9964@gmail.com>
2022-06-02 00:36:35 +0900

    Feat: write diary 페이지 done 버튼 클릭 연결

--- app/src/main/java/com/example/roomofmemory/WriteDiary.java
+++ app/src/main/java/com/example/roomofmemory/WriteDiary.java
@@ -2,13 +2,27 @@ package com.example.roomofmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WriteDiary extends AppCompatActivity {
+
+    Button doneWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_diary);
+
+        doneWrite = (Button) findViewById(R.id.doneWrite);
+        doneWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DateDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 }
