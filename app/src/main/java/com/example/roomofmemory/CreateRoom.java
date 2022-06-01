1a18c6a11c94ebd04881f81e69052a1448b3ddd4
권정솔 <jskwon@gwonjeongsol-ui-MacBookPro.local>
2022-06-01 20:21:10 +0900

    commit: message list

---
+++ app/src/main/java/com/example/roomofmemory/CreateRoom.java
@@ -0,0 +1,39 @@
+package com.example.roomofmemory;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CreateRoom extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.tool_home:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_room);
+
+        // Toolbar를 액티비티의 App Bar로 지정
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+
+        //App Bar 제어를 위해 툴바 액세스
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Creating Room");
+    }
+}
---
+++ app/src/main/java/com/example/roomofmemory/RoomsActivity.java
@@ -0,0 +1,40 @@
+package com.example.roomofmemory;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RoomsActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.tool_home:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms);
+
+        // Toolbar를 액티비티의 App Bar로 지정
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+
+        //App Bar 제어를 위해 툴바 액세스
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Rooms");
+    }
+}
