2f19197f6baef054f58eaa8118a12185505dfc77
hyowon <hyowon9964@gmail.com>
2022-06-01 23:59:55 +0900

    Design: write diary 페이지 레이아웃

---
+++ app/src/main/java/com/example/roomofmemory/WriteDiary.java
@@ -0,0 +1,14 @@
+package com.example.roomofmemory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteDiary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_diary);
+    }
+}
